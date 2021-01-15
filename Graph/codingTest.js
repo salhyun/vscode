18d799fceb72b6705a4a551ea9503c01671d39be
salhyun <salhyun@naver.com>
2021-01-15 18:12:11 +0900

    updated 정렬들.js

--- Graph/codingTest.js
+++ Graph/codingTest.js
@@ -1,3 +1,67 @@
+let system=16;
+let count=16;
+let people=2;
+let order=1;
+let result=[];
+
+if(system == 2 || system == 16) {
+  let length = people*count;
+  let data=[];
+  for(let i=0; i<length; i++) {
+    let a = i.toString(system);
+    for(let n=0; n<a.length; n++) {
+      data.push(a[n]);
+    }
+  }
+  for(let i=0; i<data.length; i++) {
+    if(i%people === (order-1)) {
+      result.push(data[i]);
+    }
+  }
+}
+console.log(`result=${result}`);
+
+let message = 'KAKAO';
+let start = 'A'.charCodeAt(0);
+let end = 'Z'.charCodeAt(0);
+let indices = new Map();
+for(let i=start; i<end; i++) {
+  indices.set( String.fromCharCode(i), i);
+}
+
+function checkIndex(s) {
+  let result = false;
+  if(indices.get(s)) {
+    console.log(s + ' code = ' + (indices.get(s) - start));
+    result = true;
+  }
+  return result;
+}
+function setIndex(s) {
+  indices.set(s, start+indices.size);
+}
+let pos=0;
+let a = checkIndex(message.charAt(pos));
+a = message.charAt(pos) + message.charAt(++pos);
+setIndex(a);
+while(pos < message.length-1) {
+  a = message.charAt(pos) + message.charAt(pos+1);
+  if(checkIndex(a)) {
+    a += message.charAt(pos+2);
+    setIndex(a);
+    pos += 2;
+  } else {
+    a = message.charAt(pos);
+    checkIndex(a);
+    a = message.charAt(pos) + message.charAt(++pos);
+    setIndex(a);
+  }
+}
+checkIndex(message.charAt(pos));
+
+
+
+
 let participant = ["mislav", "stanko", "mislav", "ana"];
 let completion = ["mislav", "ana", "mislav"]
 
@@ -60,7 +124,7 @@ for(let i=0; i<answers.length; i++) {
 console.log('paper =', paper);
 let c=0;
 paper = [];
-let a = [2, 1, 2, 3, 2, 4, 2, 5];
+a = [2, 1, 2, 3, 2, 4, 2, 5];
 for(let i=0; i<answers.length; i++) {
   paper.push(a[c++]);
   if(c >= a.length) c=0;
--- Graph/problems/정렬/정렬들.js
+++ Graph/problems/정렬/정렬들.js
@@ -1,4 +1,6 @@
-let data = [9, 8, 1, 3, 2];
+let step = 0;
+//let data = [9, 8, 1, 3, 2];
+let data = [24,5,20,28,33,1,44,43,17,47,21,36,29,6,26,7,10,49,19,11,30,2,48,34,12,45,39,23,16,46,27,25,3,40,4,35,31,14,42,15,9,38,41,8,0,18,37,22,13,32];
 //선택정렬
 function SelectionSort(data) {
   for(let i=0; i<data.length; i++) {
@@ -9,12 +11,15 @@ function SelectionSort(data) {
       }
     }
     [data[i], data[minIndex]] = [data[minIndex], data[i]];
+    console.log(`step[${++step}] data=${data}`);
   }
 }
 SelectionSort(data);
 console.log('selectionSort =', data);
 //삽입정렬
-data = [9, 8, 1, 3, 2];
+step = 0;
+//data = [9, 8, 1, 3, 2];
+data = [24,5,20,28,33,1,44,43,17,47,21,36,29,6,26,7,10,49,19,11,30,2,48,34,12,45,39,23,16,46,27,25,3,40,4,35,31,14,42,15,9,38,41,8,0,18,37,22,13,32];
 function InsertSort(data) {
   for(let i=1; i<data.length; i++) {
     let key=i;
@@ -22,6 +27,7 @@ function InsertSort(data) {
       let prev = key-1;
       if(data[key] < data[prev]) {
         [data[key], data[prev]] = [data[prev], data[key]];
+        console.log(`step[${++step}] data=${data}`);
       }
       key--;
     }
@@ -30,43 +36,46 @@ function InsertSort(data) {
 InsertSort(data);
 console.log('InsertSort =', data);
 //퀵정렬
+step = 0;
 //data = [9, 8, 1, 3, 2];
 //data = [5, 1, 6, 3, 4, 2, 7];
-data = [38, 27, 43, 3, 9, 82, 10];
+//data = [38, 27, 43, 3, 9, 82, 10];
+//data = [3, 9, 4, 7, 5, 0, 1, 6, 8, 2];
+data = [];
+for(let i=0; i<50; i++) {
+  data.push(i);
+}
+for(let i=data.length-1; i> 0; i--) {
+  let n = Math.floor(Math.random()*(i+1));
+  [data[i], data[n]] = [data[n], data[i]];
+}
+console.log(`shuffle = ${data}`);
+data = [24,5,20,28,33,1,44,43,17,47,21,36,29,6,26,7,10,49,19,11,30,2,48,34,12,45,39,23,16,46,27,25,3,40,4,35,31,14,42,15,9,38,41,8,0,18,37,22,13,32];
+
 function QuickSort(data, start, end) {
-  let diff = end-start;
-  if(diff < 2) {
+  function swap(a, b) {
+    [data[a], data[b]] = [data[b], data[a]];
+    console.log(`step[${++step}] data=${data}`);
+  }
+  let diff = end-start+1;//인덱스이기 때문에 +1 해서 갯수로 해줘야 함
+  if(diff <= 2) {
     if(data[start] > data[end]) {
-      [data[start], data[end]] = [data[end], data[start]];
+      swap(start, end);
     }
   } else {
     let left=start, right=end;
-    let pivot = parseInt(start+(diff/2+0.5));
+    let pivot = data[parseInt(start+diff/2)];
 
-    function swap(a, b) {
-      [data[a], data[b]] = [data[b], data[a]];
-    }
     while(left <= right) {
-      if(right === pivot) {
-        swap(left, right);
-        pivot = left;
-        left++;
-        right--;
-      } else if(left === pivot) {
-        swap(left, right);
-        pivot = right;
-        left++;
-        right--;
-      }
-      if(data[left] > data[pivot] && data[pivot] > data[right]) {
-        swap(left, right);
-        left++;
-        right--;
-      }
-      if(data[left] < data[pivot]) {
+      while(data[left] < pivot) {
         left++;
       }
-      if(data[right] > data[pivot]) {
+      while(data[right] > pivot) {
+        right--;
+      }
+      if(left <= right) {
+        swap(left, right);
+        left++;
         right--;
       }
     }
@@ -74,5 +83,6 @@ function QuickSort(data, start, end) {
     QuickSort(data, left, end);
   }
 }
+console.log(`step[${++step}] data=${data}`);
 QuickSort(data, 0, data.length-1);
-console.log('QuickSort =', data);
+console.log(`QuickSort =${data}`);
